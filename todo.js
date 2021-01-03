73860d608c86f7b952214904fd6a4ee9a97f2cbc
Egga <57639719+eggstrema@users.noreply.github.com>
2021-01-03 12:56:49 +0100

    add save and load functions

--- todo.js
+++ todo.js
@@ -11,5 +11,24 @@ angular.module('todoApp', [])
       todoList.todos.push({text:todoList.todoText});
       todoList.todoText = '';
     };
- 
+	
+    todoList.save = function() {
+	  clone = [];
+	  for (let i = 0; i < todoList.todos.length; i++) {
+		clone.push({ text: todoList.todos[i].text});
+	  }
+      serialized = JSON.stringify(clone);
+      localStorage.setItem("todos", serialized);
+
+    };
+	
+    todoList.load = function() {
+	  serialized = localStorage.getItem("todos");
+	  
+      list = JSON.parse(serialized);;
+	  for (let i = 0; i < list.length; i++) {
+        todoList.todos.push({text:list[i].text});
+	  }
+    };
+	 
   });
