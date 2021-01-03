e4f4edd2b36b41a17d8cc8c0265a478c7fc0eb29
Egga <57639719+eggstrema@users.noreply.github.com>
2021-01-03 13:16:40 +0100

    automatically load and save todos

--- todo.js
+++ todo.js
@@ -1,15 +1,12 @@
 angular.module('todoApp', [])
   .controller('TodoListController', function() {
     var todoList = this;
-    todoList.todos = [
-      {text:'take out the trash'},
-      {text:'do the laundry'},
-      {text:'call my dentist'},
-      {text:'go for a run'}];
- 
+    todoList.todos = [];
+	
     todoList.addTodo = function() {
       todoList.todos.push({text:todoList.todoText});
       todoList.todoText = '';
+	  todoList.save();
     };
 	
     todoList.save = function() {
@@ -21,14 +18,23 @@ angular.module('todoApp', [])
       localStorage.setItem("todos", serialized);
 
     };
-	
-    todoList.load = function() {
+
+	todoList.load = function() {
 	  serialized = localStorage.getItem("todos");
-	  
+	  if (!serialized) { return; }
       list = JSON.parse(serialized);;
 	  for (let i = 0; i < list.length; i++) {
         todoList.todos.push({text:list[i].text});
 	  }
     };
-	 
+
+	todoList.load();
+	
+	if (todoList.todos.length == 0) {
+	  todoList.todos.push({text:'take out the trash'});
+	  todoList.todos.push({text:'do the laundry'});
+	  todoList.todos.push({text:'call my dentist'});
+	  todoList.todos.push({text:'go for a run'});
+	}
+		 
   });
