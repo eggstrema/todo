a45591946fd3ef7f65ad1069a54a576df83c1933
Egga <57639719+eggstrema@users.noreply.github.com>
2021-01-03 13:46:09 +0100

    refactor

--- todo.js
+++ todo.js
@@ -6,35 +6,36 @@ angular.module('todoApp', [])
     todoList.addTodo = function() {
       todoList.todos.push({text:todoList.todoText});
       todoList.todoText = '';
-	  todoList.save();
+      todoList.save();
     };
 	
     todoList.save = function() {
-	  clone = [];
-	  for (let i = 0; i < todoList.todos.length; i++) {
-		clone.push({ text: todoList.todos[i].text});
-	  }
+      clone = [];
+      for (let i = 0; i < todoList.todos.length; i++) {
+        clone.push({ text: todoList.todos[i].text});
+	    }
       serialized = JSON.stringify(clone);
       localStorage.setItem("todos", serialized);
 
     };
 
-	todoList.load = function() {
-	  serialized = localStorage.getItem("todos");
-	  if (!serialized) { return; }
-      list = JSON.parse(serialized);;
-	  for (let i = 0; i < list.length; i++) {
-        todoList.todos.push({text:list[i].text});
-	  }
+    todoList.load = function() {
+      serialized = localStorage.getItem("todos");
+
+      if (serialized) { 		   
+        list = JSON.parse(serialized);;
+        for (let i = 0; i < list.length; i++) {
+          todoList.todos.push({text:list[i].text});
+        }
+
+      } else {
+        todoList.todos.push({text:'take out the trash'});
+        todoList.todos.push({text:'do the laundry'});
+        todoList.todos.push({text:'call my dentist'});
+        todoList.todos.push({text:'go for a run'});
+      }
     };
 
-	todoList.load();
-	
-	if (todoList.todos.length == 0) {
-	  todoList.todos.push({text:'take out the trash'});
-	  todoList.todos.push({text:'do the laundry'});
-	  todoList.todos.push({text:'call my dentist'});
-	  todoList.todos.push({text:'go for a run'});
-	}
-		 
+    todoList.load();
+			 
   });
